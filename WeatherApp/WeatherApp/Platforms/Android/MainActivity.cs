620a312ee8889033278878c25eb0eb497e3bdbae
Sahan <sahanshamin@gmail.com>
2024-11-17 01:31:23 +0800

    Feat: fullscreen
    unsure if working, emulator shows black bar, likely emulator problem, cannot currently test if behaviour is correct

--- WeatherApp/WeatherApp/Platforms/Android/MainActivity.cs
+++ WeatherApp/WeatherApp/Platforms/Android/MainActivity.cs
@@ -1,11 +1,19 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using Android.Views;
 
 namespace WeatherApp
 {
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Make the app fullscreen
+            Window.SetFlags(WindowManagerFlags.Fullscreen, WindowManagerFlags.Fullscreen);
+        }
     }
 }
